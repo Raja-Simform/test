2d8df8888e089ec748f643582734c49a0b58b3b0
Raja-Simform <raja.jha@simformsolutions.com>
2025-04-23 15:36:00 +0530

    add js 2

--- prac2.js
+++ prac2.js
@@ -1,31 +1,34 @@
+ function path(arr){
+     let obj={};
+     for(let a of arr){
+       let pathstr=a["path"].split('.');
+       let value=a["value"];
+       let temp=obj;
+       for(let b=0;b<pathstr.length-1;b++){
+        if(!temp[pathstr[b]]){
+         temp[pathstr[b]]={}
+        }
+        temp=temp[pathstr[b]];        
+        }
+        temp[pathstr[pathstr.length-1]]=value;
+     }
+     return obj;
+  }
 
-function path(arr){
-    let obj={};
-    
-    for(let a of arr){
-      let path=a["path"].split('.');
-      let val=a["value"];
-      let temp=obj;
-
-      for(let b=0;b<path;b++){
-         console.log(a[b]);
-      }
- 
-    }
-    
-}
-const paths = [
-    { path: "user.name.first", value: "John" }
+  const paths = [
+    { path: "user.name.first", value: "John" },
+    { path: "user.name.last", value: "Doe" },
+    { path: "user.age", value: 30 }
   ];
- path(paths); 
+  console.log(path(paths));
   
-//   output:
-//   {
-//     user: {
-//       name: {
-//         first: "John",
-//         last: "Doe"
-//       },
-//       age: 30
-//     }
-//   }
+  // output:
+  // {
+  //   user: {
+  //     name: {
+  //       first: "John",
+  //       last: "Doe"
+  //     },
+  //     age: 30
+  //   }
+  // }
